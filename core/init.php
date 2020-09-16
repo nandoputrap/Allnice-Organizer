f354c9e36db88b474d1565d700bb38e0a8c13fa9
ndo <nandoputrap@gmail.com>
2020-09-16 12:49:08 +0700

    convert php to html done

--- core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  include_once 'functions/config.php';
-  include_once 'view/header.php';
-  include_once 'fungsi.php';
-
-?>
--- sign-out.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('location:sign-in.php');//jika berhasil maka akan kembali ke login form
-?>
